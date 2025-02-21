f2544fe40b6fef7100e2e1c12da4b6e7f681527c
devRezaulKarim <dev.rezaulkarim@gmail.com>
2025-02-21 19:47:58 +0600

    FEAT: Add Prisma schema and generate migration file
    - Defined all necessary models in the Prisma schema, including relationships and field constraints.
    - Generated the initial migration file to apply database schema changes.
    - Ensured proper data types and indexing for optimized queries.
    - Prepared the database structure for seamless integration with the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Shop infinity",
-  description: "Shop for happiness",
+  title: "Carz",
+  description:
+    "Carz is an e-commerce platform for buying and selling cars, featuring user-friendly browsing, advanced search filters, secure payments, and a responsive design. It allows users to compare cars, manage profiles, and track purchases, while providing an intuitive admin panel for managing listings.",
 };
 
 export default function RootLayout({
