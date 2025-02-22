a96d980e44edb986a6cfc73a3078042d01e2aaf5
devRezaulKarim <dev.rezaulkarim@gmail.com>
2025-02-22 08:13:46 +0600

    FEAT: Install Faker and Slugify, seed database with classified data
    - Installed `faker` to generate realistic dummy data for classified entries.
    - Installed `slugify` to generate unique slugs from title and VRM.
    - Created a script to generate and seed classified data into the database.
    - Implemented slug generation to ensure unique and SEO-friendly URLs.
    - Ensured proper relationships and data integrity while seeding.
    - Optimized the seeding process for efficiency and scalability.
    - Verified successful insertion of classified data into the database.

---
+++ prisma/seed/classified.seed.ts
@@ -0,0 +1,80 @@
+import { faker } from "@faker-js/faker";
+import {
+  BodyType,
+  ClassifiedStatus,
+  Color,
+  CurrencyCode,
+  FuelType,
+  OdoUnit,
+  Prisma,
+  PrismaClient,
+  Transmission,
+  ULEZCompliance,
+} from "@prisma/client";
+import slugify from "slugify";
+
+export async function seedClassified(prisma: PrismaClient) {
+  const makes = await prisma.make.findMany({
+    include: {
+      models: {
+        include: {
+          modelVariants: true,
+        },
+      },
+    },
+  });
+
+  const classifiedsData: Prisma.ClassifiedCreateManyInput[] = [];
+  for (let i = 0; i < 25; i++) {
+    const make = faker.helpers.arrayElement(makes);
+    if (!make.models.length) continue;
+    const model = faker.helpers.arrayElement(make.models);
+
+    const variant = model.modelVariants.length
+      ? faker.helpers.arrayElement(model.modelVariants)
+      : null;
+
+    const year = faker.date
+      .between({
+        from: new Date(1925, 0, 1),
+        to: new Date(),
+      })
+      .getFullYear();
+    const title = [year, make.name, model.name, variant?.name]
+      .filter(Boolean)
+      .join(" ");
+
+    const vrm = faker.vehicle.vrm();
+    const baseSlug = slugify(`${title} ${vrm}`);
+
+    classifiedsData.push({
+      year,
+      vrm,
+      slug: baseSlug,
+      makeId: make.id,
+      modelId: model.id,
+      ...(variant?.id && { modelVariantId: variant.id }),
+      title,
+      price: faker.number.int({ min: 400000, max: 10000000 }),
+      odoReading: faker.number.int({ min: 0, max: 20000 }),
+      doors: faker.number.int({ min: 2, max: 8 }),
+      seats: faker.number.int({ min: 2, max: 8 }),
+      views: faker.number.int({ min: 5, max: 1000 }),
+      description: faker.commerce.productDescription(),
+      currency: faker.helpers.arrayElement(Object.values(CurrencyCode)),
+      odoUnit: faker.helpers.arrayElement(Object.values(OdoUnit)),
+      bodyType: faker.helpers.arrayElement(Object.values(BodyType)),
+      transmission: faker.helpers.arrayElement(Object.values(Transmission)),
+      fuelType: faker.helpers.arrayElement(Object.values(FuelType)),
+      color: faker.helpers.arrayElement(Object.values(Color)),
+      ulezCompliance: faker.helpers.arrayElement(Object.values(ULEZCompliance)),
+      status: faker.helpers.arrayElement(Object.values(ClassifiedStatus)),
+    });
+  }
+
+  const result = await prisma.classified.createMany({
+    data: classifiedsData,
+    skipDuplicates: true, //To prevent any duplicate errors from duplicate slugs
+  });
+  console.log(`Seeded ${result.count} classifieds 🌱`);
+}
--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { seedTaxonomy } from "./taxonomy.seed";
+import { seedClassified } from "./classified.seed";
 const prisma = new PrismaClient();
 const main = async () => {
   await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE;`;
   await seedTaxonomy(prisma);
+  await seedClassified(prisma);
 };
 
 main()
